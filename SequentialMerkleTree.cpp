873e20e9bf70b1c6f11491a4035d5b254ef40ebe
unknown <Dgomez.1730@hotmail.com>
2020-03-21 22:53:22 -0400

    Uploading the properly functioning merkle tree

--- SequentialMerkleTree.cpp
+++ SequentialMerkleTree.cpp
@@ -37,7 +37,7 @@ MerkleNode::MerkleNode(size_t _hash) {
 }
 
 MerkleNode::MerkleNode() {
-  hash = NULL;
+  hash = -1;
   left = NULL;
   right = NULL;
 }
@@ -46,60 +46,54 @@ class MerkleTree {
 public:
   MerkleNode root;
 
-
+  MerkleTree(MerkleNode _root);
 };
 
-MerkleNode recursivePopulate(vector<MerkleNode> hashedNodes, hash<size_t> hash) {
-  vector<MerkleNode> upperLevel;
+MerkleTree::MerkleTree(MerkleNode _root) {
+  root = _root;
+}
 
+MerkleNode* recursivePopulate(vector<MerkleNode*> hashedNodes, hash<size_t> hash) {
+  vector<MerkleNode*> upperLevel;
+  cout << "D: Recursing through hashedNodes with size (" << hashedNodes.size() << ")" << endl;
   if(hashedNodes.size() == 1){
-    cout << "Root hash: " << hashedNodes.at(0).hash << " Left Hash: " << hashedNodes.at(0).left->hash << " Right Hash: " << hashedNodes.at(0).right->hash << endl;
-    return hashedNodes.at(0); // I believe this returns the root once it was created in the last cycle
+    cout << "D: Root Node has been reached at address (" << hashedNodes.at(0) << ") and hash (" << hashedNodes.at(0)->hash << ")" << endl;
+    return hashedNodes.at(0);
   }
 
   for (int i = 0; i < hashedNodes.size() - 1; i = i + 2) {
-    cout << "MN: " << i  << endl;
+    MerkleNode* node = new MerkleNode(hash((hashedNodes.at(i))->hash + (hashedNodes.at(i+1))->hash));
 
-    cout << "Left Hashed Node: " << (&hashedNodes.at(i))->hash << endl;
-    MerkleNode node(hash((&hashedNodes.at(i))->hash + (&hashedNodes.at(i+1))->hash));
-
-    node.left = &hashedNodes.at(i);
-    node.right = &hashedNodes.at(i+1);
-
-    cout << "LEFT NODE AFTER HASH: " << node.left->hash << endl;
+    node->left = hashedNodes.at(i);
+    node->right = hashedNodes.at(i+1);
 
     upperLevel.push_back(node);
   }
 
   if(hashedNodes.size() % 2 == 1) {
-    MerkleNode node(hash(hashedNodes.at(hashedNodes.size()-1).hash));
+    MerkleNode* node = new MerkleNode(hash(hashedNodes.at(hashedNodes.size()-1)->hash));
 
-    node.left = &hashedNodes.at(hashedNodes.size()-1);
+    node->left = hashedNodes.at(hashedNodes.size()-1);
     upperLevel.push_back(node);
   }
 
-
-  for (int i = 0; i < upperLevel.size(); i++)
-    cout << upperLevel.at(i).hash << " ";
-  cout << endl;
-
-  cout << "Root hash: " << upperLevel.at(0).hash << " Left Hash: " << upperLevel.at(0).left->hash << " Right Hash: " << upperLevel.at(0).right->hash << endl;
   return recursivePopulate(upperLevel, hash);
 }
 
 // Returns root of merkle tree
 template <typename T>
-MerkleNode populate(vector<LeafNode<T> > leaves) {
-  vector<MerkleNode> base;
+MerkleNode* populate(vector<LeafNode<T> > leaves) {
+  cout << "D: Entering Populate Function" << endl;
+  vector<MerkleNode*> base;
   hash<size_t> hash;
 
   for (int i = 0; i < leaves.size(); i++) {
-    cout << "Leaf: " << i  << endl;
-    MerkleNode node(leaves.at(i).hash);
+    MerkleNode* node = new MerkleNode(leaves.at(i).hash);
 
     base.push_back(node);
   }
 
+  cout << "D: Entering Recursive Function" << endl;
   return recursivePopulate(base, hash);
 }
 
@@ -118,24 +112,20 @@ int main() {
   size_t hashB = test(B);
   size_t hashC = test(C);
   size_t hashD = test(D);
-  //cout << resHash + resHash << endl;
 
-  LeafNode<string> tempA(A, hashA);
-  LeafNode<string> tempB(B, hashB);
-  LeafNode<string> tempC(C, hashC);
-  LeafNode<string> tempD(D, hashD);
+  LeafNode<int> tempA(1, hashA);
+  LeafNode<int> tempB(2, hashB);
+  LeafNode<int> tempC(3, hashC);
+  LeafNode<int> tempD(4, hashD);
 
-  vector<LeafNode<string> > result;
+  vector<LeafNode<int> > result;
   result.push_back(tempA);
   result.push_back(tempB);
   result.push_back(tempC);
   result.push_back(tempD);
 
-  cout << "TempA hash: " << hashA << " TempB hash: " << hashB << endl;
+  MerkleNode* root = populate(result);
 
-  MerkleNode root = populate(result);
-
-  cout << "Root hash: " << root.hash << " Left Hash: " << root.left->hash << " Right Hash: " << root.right->hash << endl;
-
+  cout << "Root hash: " << root->hash << " Left Hash: " << root->left->hash << " Right Hash: " << root->right->hash << endl;
 
 }
