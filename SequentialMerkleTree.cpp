d0a7b5ffaf3ddd414e0908ca49c557b5207ac646
unknown <Dgomez.1730@hotmail.com>
2020-03-23 17:29:49 -0400

    Both trees have been completed with compare prints

--- SequentialMerkleTree.cpp
+++ SequentialMerkleTree.cpp
@@ -44,19 +44,16 @@ MerkleNode::MerkleNode() {
   right = NULL;
 }
 
+// Helper function for populate()
 MerkleNode* recursivePopulate(vector<MerkleNode*> hashedNodes, hash<string> hash) {
   vector<MerkleNode*> upperLevel;
-  cout << "D: Recursing through hashedNodes with size (" << hashedNodes.size() << ")" << endl;
   if(hashedNodes.size() == 1){
-    cout << "D: Root Node has been reached at address (" << hashedNodes.at(0) << ") and hash (" << hashedNodes.at(0)->hash << ")" << endl;
     return hashedNodes.at(0);
   }
 
   for (int i = 0; i < hashedNodes.size() - 1; i = i + 2) {
 
     stringstream ss;
-    cout << "Hash 1: " << (hashedNodes.at(i))->hash << endl;
-    cout << "Hash 2: " << (hashedNodes.at(i+1))->hash << endl;
     ss << (hashedNodes.at(i))->hash << (hashedNodes.at(i+1))->hash;
     stringstream output(ss.str());
 
@@ -87,10 +84,8 @@ MerkleNode* recursivePopulate(vector<MerkleNode*> hashedNodes, hash<string> hash
 // Returns root of merkle tree
 template <typename T>
 MerkleNode* populate(vector<LeafNode<T> > leaves) {
-  cout << "D: Entering Populate Function" << endl;
   vector<MerkleNode*> base;
   hash<string> hash;
-
 
   for (int i = 0; i < leaves.size(); i++) {
     MerkleNode* node = new MerkleNode(leaves.at(i).hash);
@@ -98,20 +93,19 @@ MerkleNode* populate(vector<LeafNode<T> > leaves) {
     base.push_back(node);
   }
 
-  cout << "D: Entering Recursive Function" << endl;
   return recursivePopulate(base, hash);
 }
 
+// Compares two vectors of leaf values
 template <typename T>
 bool validate(vector<LeafNode<T> > a1, vector<LeafNode<T> > a2) {
   MerkleNode* firstRoot = populate(a1);
   MerkleNode* secondRoot = populate(a2);
 
-  cout << "Hash 1: " << firstRoot->hash << endl;
-  cout << "Hash 2: " << secondRoot->hash << endl;
   return (firstRoot->hash.compare(secondRoot->hash) == 0);
 }
 
+// Inserted a new Merkle Node into the right most of the root
 MerkleNode* insertLeaf(MerkleNode* root, string passedHash) {
   MerkleNode* temp = root;
   MerkleNode* insertedNode = new MerkleNode(passedHash);
@@ -125,7 +119,6 @@ MerkleNode* insertLeaf(MerkleNode* root, string passedHash) {
     if (temp->left == NULL && temp->right == NULL) {
       leafMerkles.push_back(temp);
     } else if (temp->right == NULL) {
-      cout << "Hit?" << endl;
       leafMerkles.push_back(temp->left);
     } else {
       q.push(temp->left);
@@ -142,17 +135,14 @@ int main() {
 
   // Create list of nodes with precreated hashes through hash<T>
   // populate Merkle Tree with list
-
   hash<string> test;
 
   string A = "A";
   string B = "B";
   string C = "C";
-  string D = "D";
 
   stringstream convert;
   convert << test(A);
-  cout << convert.str() << endl;
   string hashA = convert.str();
 
   convert.str("");
@@ -163,32 +153,20 @@ int main() {
   convert << test(C);
   string hashC = convert.str();
 
-  convert.str("");
-  convert << test(D);
-  string hashD = convert.str();
-
   LeafNode<int> tempA(1, hashA);
   LeafNode<int> tempB(2, hashB);
   LeafNode<int> tempC(3, hashC);
-  LeafNode<int> tempD(4, hashD);
 
   vector<LeafNode<int> > result;
   result.push_back(tempA);
   result.push_back(tempB);
-  //result.push_back(tempC);
-  //result.push_back(tempD);
 
   vector<LeafNode<int> > tester;
   tester.push_back(tempA);
   tester.push_back(tempB);
   tester.push_back(tempC);
-  //tester.push_back(tempC);
 
   MerkleNode* root = populate(result);
-  MerkleNode* newRoot = insertLeaf(root, "123");
-
-  cout << validate(result, tester) << endl;
-  cout << "Root hash: " << root->hash << " Left Hash: " << root->left->hash << " Right Hash: " << root->right->hash << endl;
-  cout << "NR hash: " << newRoot->hash << " NR Left Hash: " << newRoot->left->left->hash << " NR Right Hash: " << newRoot->right->left->hash << endl;
-
+  MerkleNode* newRoot = insertLeaf(root, "D");
+  cout << "Are the trees the same: " << validate(result, tester) << endl;
 }
